ec22aac9d051142dbf267c79f34fdd892d1dfb75
Bucciamarcia <info@stefanomini.com>
2026-05-08 17:25:48 +0200

    Lists messages

--- frontend/src/routes/chat/+page.server.ts
+++ frontend/src/routes/chat/+page.server.ts
@@ -2,6 +2,8 @@ import type { ChatMessagesResponse, UsersResponse } from "$lib/pocketbase-types"
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const resultList = locals.pb.collection("chat_messages")
+	const resultList = await locals.pb.collection("chat_messages")
 		.getList<ChatMessagesResponse<{ author: UsersResponse }>>(1, 20, { expand: "author" });
+
+	return { messages: resultList.items }
 }
---
+++ frontend/src/routes/chat/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	let { data }: { data: PageData } = $props();
+</script>
+
+{#each data.messages as message}
+	<p>created: {message.created}</p>
+	<p>body: {message.body}</p>
+	<p>author: {message.expand?.author.username}</p>
+{/each}
