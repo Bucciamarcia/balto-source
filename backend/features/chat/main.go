ed82ebfedffec9fd6fbbbd1cc510c76ea2a43cc5
Bucciamarcia <info@stefanomini.com>
2026-05-08 17:59:20 +0200

    Backend to save chat message

---
+++ backend/features/chat/main.go
@@ -0,0 +1,22 @@
+package chat
+
+import (
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func InsertChatMessage(content string, author string, app *pocketbase.PocketBase) error {
+	// TODO: Add chat message
+	collection, err := app.FindCollectionByNameOrId("chat_messages")
+	if err != nil {
+		return err
+	}
+	record := core.NewRecord(collection)
+	record.Set("body", content)
+	record.Set("author", author)
+	err = app.Save(record)
+	if err != nil {
+		return err
+	}
+	return nil
+}
