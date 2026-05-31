5a97d42569e43d4210c001782edb509757b9a219
jake <jacob36926@gmail.com>
2026-05-30 20:19:35 -0400

    Created the text for the side menu
    Added a temp banner from the old BS
    
    Added footer text from BS
    
    Added a title to the homepage

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,23 +1,34 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
-	import type { PageData } from './$types';
+    import {enhance} from '$app/forms';
+    import type {PageData} from './$types';
 
-	let { data }: { data: PageData } = $props();
+    let {data}: { data: PageData } = $props();
 </script>
 
-<h1>We are so back</h1>
-<br />
-<br />
+<title>Balto Source - Reach For The Light</title>
+
+<h1>Balto Source: We are so back!!!!!</h1>
+<br/>
+<br/>
 <p><a href="/signup">sign up</a></p>
 <p><a href="/login">login</a></p>
 
 {#each data.resultList as news}
-	<p>title: {news.title}</p>
-	<p>body: {news.body}</p>
-	<p>author: {news.expand?.author.username}</p>
-	<p>created: {news.created}</p>
+    <p>title: {news.title}</p>
+    <p>body: {news.body}</p>
+    <p>author: {news.expand?.author.username}</p>
+    <p>created: {news.created}</p>
 {/each}
 
-<form method="POST" action="?/logout" use:enhance>
-	<button type="submit">Logout</button>
+<form action="?/logout" method="POST" use:enhance>
+    <button type="submit">Logout</button>
 </form>
+
+<style>
+
+    h1 {
+        text-align: center;
+        font-size: 50px;
+        color: lightblue;
+    }
+</style>
