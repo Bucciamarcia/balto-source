417a26abdead542a32d5507e79a99d70d85a0cfb
Stefano Mini <info@stefanominiconsulting.com>
2026-06-25 17:40:25 +0300

    Adds date

---
+++ frontend/src/lib/components/FormattedDate.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { format } from 'date-fns';
+
+	function formatDate(d: Date): string {
+		return format(d, 'MMM d, yyyy');
+	}
+
+	let { date }: { date: Date } = $props();
+</script>
+
+{formatDate(date)}
--- frontend/src/routes/SingleNews.svelte
+++ frontend/src/routes/SingleNews.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import FormattedDate from '$lib/components/FormattedDate.svelte';
 	import type { HomepageNewsResponse, UsersResponse } from '$lib/pocketbase-types';
 	import { PUBLIC_POCKETBASE_URL } from '$lib/pocketbase/url';
 
@@ -15,18 +16,21 @@
 
 <div class="border">
 	<h2>{news.title}</h2>
-	<div class="flex">
+	<div class="mt-4 mb-4 flex">
 		<div class="self-center">
 			<p>By {news.expand.author.username}</p>
 		</div>
 		{#if news.expand.author.avatar != ''}
-			<div class="ml-2">
+			<div class="mr-2 ml-2">
 				<img
 					src={buildAvatarUrl(news.expand.author)}
 					alt={buildUserAvatarTag(news.expand.author)}
 					height="40"
 					width="40"
 				/>
+			</div>
+			<div class="self-center">
+				<p>On <FormattedDate date={new Date()} /></p>
 			</div>
 		{/if}
 	</div>
