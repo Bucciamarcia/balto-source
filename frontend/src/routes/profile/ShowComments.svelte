a6b090dfc04050f86647cddcae0e7ee6e50be496
Stefano Mini <info@stefanominiconsulting.com>
2026-07-13 15:22:57 +0300

    Shows avatar and username

--- frontend/src/routes/profile/ShowComments.svelte
+++ frontend/src/routes/profile/ShowComments.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
+	import { buildAvatarUrl } from '$lib/components/buildAvatarUrl';
 	import type { CommentsResponse, UsersResponse } from '$lib/pocketbase-types';
 
 	let { comments }: { comments: CommentsResponse<{ author: UsersResponse }>[] } = $props();
 </script>
 
 {#each comments as comment}
-	<p>{comment.id}</p>
+	<div class="mt-5 border-2 border-accent p-5">
+		<div class="flex">
+			<div class="mr-5 self-center">{comment.expand.author.username}</div>
+			<div>
+				<img
+					src={buildAvatarUrl(comment.expand.author)}
+					alt="{comment.expand.author.username} avatar"
+				/>
+			</div>
+		</div>
+	</div>
 {/each}
