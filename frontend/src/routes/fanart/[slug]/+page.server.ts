2a297fffb80cefd604beaf493a1ebddd3bda3c56
Stefano Mini <info@stefanominiconsulting.com>
2026-07-17 20:55:05 +0300

    Can add and remove from favs

--- frontend/src/routes/fanart/[slug]/+page.server.ts
+++ frontend/src/routes/fanart/[slug]/+page.server.ts
@@ -3,19 +3,55 @@ import { fail } from "@sveltejs/kit";
 import type { PageServerLoad, Actions } from "./$types";
 
 export const load: PageServerLoad = async ({ locals, params }) => {
+	async function hasUserAlreadyFaved(userId: string, fanartId: string): Promise<boolean> {
+		const result = await locals.pb.collection("fanart_favorites").getFullList({
+			filter: `source = "${userId}" && target = "${fanartId}"`
+		});
+		if (result.length != 0) {
+			return true;
+		}
+		return false;
+	}
 	const id = params.slug;
 	const fanart = await locals.pb.collection("fanarts").getOne<FanartsResponse<{ author: UsersResponse }>>(id, { expand: "author" });
 	const favs = await locals.pb.collection("fanart_favorites").getFullList<FanartFavoritesResponse>({
 		filter: `target="${id}"`
 	})
-	return { fanart, favs }
+	const user = locals.user;
+	const alreadyFaved = user == null ? false : await hasUserAlreadyFaved(user.id, fanart.id);
+	return { fanart, favs, user, alreadyFaved }
 }
 
 export const actions = {
 	favorite: async (event) => {
 		const user = event.locals.user;
 		if (user == null) {
-			return fail(400, { "error": "Not logged in" });
+			return fail(400, { error: "Not logged in" });
+		}
+		const fanartId = event.params.slug;
+		try {
+			await event.locals.pb.collection("fanart_favorites").create({
+				source: user.id, target: fanartId
+			});
+		} catch (e) {
+			return fail(400, { error: e instanceof Error ? e.message : "Unknown error" })
+		}
+	},
+
+	removeFavorite: async (event) => {
+		const user = event.locals.user;
+		if (user == null) {
+			return fail(44, { error: "Not logged in" })
+		}
+		const id = event.params.slug;
+		try {
+			const response = await event.locals.pb.collection("fanart_favorites").getFullList<FanartFavoritesResponse>({
+				filter: `target="${id}" && source="${user.id}"`
+			});
+			const favId: string = response[0].id;
+			await event.locals.pb.collection("fanart_favorites").delete(favId);
+		} catch (e) {
+			return fail(400, { error: e instanceof Error ? e.message : "Unknown error" })
 		}
 	}
 } satisfies Actions;
--- frontend/src/routes/fanart/[slug]/+page.svelte
+++ frontend/src/routes/fanart/[slug]/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+	import FormError from '$lib/components/formError.svelte';
 	import type { UsersResponse } from '$lib/pocketbase-types';
 	import { PUBLIC_POCKETBASE_URL } from '$lib/pocketbase/url';
 
@@ -6,6 +8,8 @@
 	// svelte-ignore state_referenced_locally
 	const author: UsersResponse = data.fanart.expand.author;
 	let isExpanded: boolean = $state(false);
+	let errorMessage: string = $state('');
+	let isLoggedIn: boolean = $derived(data.user != null);
 	function getFanartUrl(id: string, image: string): string {
 		const root = `${PUBLIC_POCKETBASE_URL}/api/files/fanarts/${id}/${image}`;
 		if (isExpanded) return root;
@@ -29,8 +33,48 @@
 			{isExpanded ? 'Show thumbnail' : 'Show full size'}
 		</button>
 		<p>{data.favs.length} favorites</p>
-		<form method="POST" action="?/favorite">
-			<button type="submit">gogo</button>
-		</form>
+		{#if isLoggedIn}
+			{#if data.alreadyFaved}
+				<form
+					method="POST"
+					action="?/removeFavorite"
+					use:enhance={() => {
+						errorMessage = '';
+						return async ({ result, update }) => {
+							await update();
+							if (result.type === 'failure') {
+								console.log(result.data);
+								errorMessage = (result.data?.error as string) ?? 'Unknown error';
+							}
+						};
+					}}
+				>
+					<button type="submit" class="btn btn-primary">Remove from favorites</button>
+					{#if errorMessage != ''}
+						<FormError message={errorMessage} />
+					{/if}
+				</form>
+			{:else}
+				<form
+					method="POST"
+					action="?/favorite"
+					use:enhance={() => {
+						errorMessage = '';
+						return async ({ result, update }) => {
+							await update();
+							if (result.type === 'failure') {
+								console.log(result.data);
+								errorMessage = (result.data?.error as string) ?? 'Unknown error';
+							}
+						};
+					}}
+				>
+					<button type="submit" class="btn btn-primary">Add to favorites</button>
+					{#if errorMessage != ''}
+						<FormError message={errorMessage} />
+					{/if}
+				</form>
+			{/if}
+		{/if}
 	</div>
 </div>
