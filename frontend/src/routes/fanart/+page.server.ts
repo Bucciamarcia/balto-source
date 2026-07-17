69fc478e8172f454faa1965bcd505a4154ee3533
Stefano Mini <info@stefanominiconsulting.com>
2026-07-17 17:11:35 +0300

    Fanart general display

--- frontend/src/routes/fanart/+page.server.ts
+++ frontend/src/routes/fanart/+page.server.ts
@@ -1,7 +1,7 @@
 import type { FanartsResponse, UsersResponse } from "$lib/pocketbase-types";
 import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async ({ locals }) => {
-	const fanarts = locals.pb.collection("fanarts").getFullList<FanartsResponse<{ author: UsersResponse }>>();
+	const fanarts = await locals.pb.collection("fanarts").getFullList<FanartsResponse<{ author: UsersResponse }>>({ expand: "author" });
 
 	return { fanarts }
 }
--- frontend/src/routes/fanart/+page.svelte
+++ frontend/src/routes/fanart/+page.svelte
@@ -1,1 +1,26 @@
-<p>hello there</p>
+<script lang="ts">
+	import { PUBLIC_POCKETBASE_URL } from '$lib/pocketbase/url';
+	import type { PageData } from './$types';
+	import FormattedDate from '$lib/components/FormattedDate.svelte';
+
+	let { data }: { data: PageData } = $props();
+
+	function getFanartUrl(id: string, image: string): string {
+		return `${PUBLIC_POCKETBASE_URL}/api/files/fanarts/${id}/${image}?thumb=300x200f`;
+	}
+</script>
+
+<div class="flex flex-wrap gap-2">
+	{#each data.fanarts as fanart}
+		<div class="flex flex-col">
+			<img src={getFanartUrl(fanart.id, fanart.image)} alt="{fanart.title} by {fanart.author}" />
+			<p class="mt-2 text-center">{fanart.title}</p>
+			<p class="text-center">
+				By <a href={`/profile?id=${fanart.author}`}>{fanart.expand.author.username}</a>
+			</p>
+			<p class="mt-2 text-center text-xs">
+				On <FormattedDate date={new Date(fanart.created)} showTime={false} />
+			</p>
+		</div>
+	{/each}
+</div>
