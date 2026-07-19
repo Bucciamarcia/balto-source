6acd8c36908d0b93f89aae3ad7113edb3f8b3902
Stefano Mini <info@stefanominiconsulting.com>
2026-07-19 22:11:48 +0300

    unverify unauth ok

--- frontend/src/lib/components/layout/head-menu/HeadMenu.svelte
+++ frontend/src/lib/components/layout/head-menu/HeadMenu.svelte
@@ -10,7 +10,7 @@
 		<LinkElement label="Sign up" destination="/signup" logOut={false} />
 	{/if}
 	<LinkElement label="Shout box" destination="/shoutbox" logOut={false} />
-	{#if isLoggedIn}
+	{#if isLoggedIn && isVerified}
 		<LinkElement label="Profile" destination="/profile" logOut={false} />
 		<LinkElement label="Upload" destination="/upload" logOut={false} />
 		<LinkElement label="Log out" destination="/" logOut={true} />
