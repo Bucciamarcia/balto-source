c5938f51095b32916a9fc680bf3d8e9b643728ff
Stefano Mini <info@stefanominiconsulting.com>
2026-07-19 18:26:06 +0300

    Adds search wait

--- frontend/src/routes/fanart/FanartSearchBox.svelte
+++ frontend/src/routes/fanart/FanartSearchBox.svelte
@@ -2,16 +2,32 @@
 	import { enhance } from '$app/forms';
 
 	let { action = '?/default' }: { action?: string } = $props();
+	let isLoading: boolean = $state(false);
 </script>
 
 <p class="mb-3 text-xl">Search</p>
-<form class="mb-5" method="POST" {action} use:enhance>
+<form
+	class="mb-5"
+	method="POST"
+	{action}
+	use:enhance={() => {
+		isLoading = true;
+		return async ({ update }) => {
+			await update();
+			isLoading = false;
+		};
+	}}
+>
 	<input
 		type="text"
 		name="filter"
 		class="input text-black"
 		placeholder="Search by author or title"
 	/>
-	<button type="submit" class="btn btn-primary">Search</button>
-	<button type="submit" formaction="?/clear" class="btn btn-error">Clear results</button>
+	{#if isLoading}
+		<span class="loading loading-sm loading-spinner"></span>
+	{:else}
+		<button type="submit" class="btn btn-primary">Search</button>
+		<button type="submit" formaction="?/clear" class="btn btn-error">Clear results</button>
+	{/if}
 </form>
