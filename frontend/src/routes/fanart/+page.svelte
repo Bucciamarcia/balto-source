d260cd8384b237bb283da5ca69c7d7865f8954b3
Stefano Mini <info@stefanominiconsulting.com>
2026-07-19 18:19:24 +0300

    Fanart search ok

--- frontend/src/routes/fanart/+page.svelte
+++ frontend/src/routes/fanart/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import FanartGallery from '$lib/components/layout/fanarts/FanartGallery.svelte';
 	import FanartSearchBox from './FanartSearchBox.svelte';
-	let { data }: { data: PageData } = $props();
+	import type { PageProps } from './$types';
+	let { data, form }: PageProps = $props();
+	let filteredFanart = $derived(form?.result);
 </script>
 
 <svelte:head>
 	<title>Fanart - Balto Source</title>
 </svelte:head>
 <FanartSearchBox action="?/filter" />
-<FanartGallery fanarts={data.fanarts} />
+<FanartGallery fanarts={filteredFanart == null ? data.fanarts : filteredFanart} />
--- frontend/src/routes/fanart/FanartSearchBox.svelte
+++ frontend/src/routes/fanart/FanartSearchBox.svelte
@@ -4,7 +4,14 @@
 	let { action = '?/default' }: { action?: string } = $props();
 </script>
 
-<p class="mb-3 text-xl">Filter the results</p>
-<form class="mb-5 gap-3" method="POST" {action} use:enhance>
-	<input type="text" name="filter" class="input" placeholder="Filter by author or title" />
+<p class="mb-3 text-xl">Search</p>
+<form class="mb-5" method="POST" {action} use:enhance>
+	<input
+		type="text"
+		name="filter"
+		class="input text-black"
+		placeholder="Search by author or title"
+	/>
+	<button type="submit" class="btn btn-primary">Search</button>
+	<button type="submit" formaction="?/clear" class="btn btn-error">Clear results</button>
 </form>
