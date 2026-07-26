5875141fc58f7b348d34afe8ddeb4e55d0898f1e
Stefano Mini <info@stefanominiconsulting.com>
2026-07-26 17:37:07 +0300

    Init notifications

--- frontend/src/lib/components/layout/head-menu/HeadMenu.svelte
+++ frontend/src/lib/components/layout/head-menu/HeadMenu.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import BellIcon from './BellIcon.svelte';
 	import LinkElement from './LinkElement.svelte';
 	let { isLoggedIn, isVerified }: { isLoggedIn: boolean; isVerified: boolean } = $props();
 </script>
@@ -14,5 +15,15 @@
 		<LinkElement label="Profile" destination="/profile" logOut={false} />
 		<LinkElement label="Upload" destination="/upload" logOut={false} />
 		<LinkElement label="Log out" destination="/" logOut={true} />
+		<div class="indicator">
+			<span class="indicator-item badge badge-error">0</span>
+			<div class="tooltip" data-tip="Notifications">
+				<button class="btn btn-primary"><BellIcon /></button>
+			</div>
+		</div>
+		<details class="dropdown dropdown-end">
+			<summary class="btn m-1">open or close</summary>
+			<div class="dropdown-content grid h-120 w-100 bg-base-300">content</div>
+		</details>
 	{/if}
 </div>
