0431b0c9917f3e31e70bafb43930724a9e0cd24a
Stefano Mini <info@stefanominiconsulting.com>
2026-09-09 19:09:37 +0300

    Adds language to locals

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -2,12 +2,22 @@ import PocketBase from 'pocketbase';
 import type { Handle } from '@sveltejs/kit';
 import { PUBLIC_POCKETBASE_URL } from '$lib/pocketbase/url';
 
+type LanguageStub = "en" | "fr";
+function getLanguageStub(url: string): LanguageStub {
+	if (url.startsWith("/en")) {
+		return "en"
+	} else {
+		return "fr"
+	}
+}
+
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.pb = new PocketBase(PUBLIC_POCKETBASE_URL);
 
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get("cookie") || "");
 
 	event.locals.auth = event.locals.pb.authStore.record;
+	event.locals.language = getLanguageStub(event.url.pathname)
 
 
 
