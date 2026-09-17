af95bda74760efb9829249e4280551f3f494c1c6
Stefano Mini <info@stefanominiconsulting.com>
2026-09-17 22:19:03 +0300

    Fix comment fanart fanfic lang

--- backend/features/notifications/comments.go
+++ backend/features/notifications/comments.go
@@ -54,11 +54,12 @@ func notifyOnFanart(app core.App, comment Comment) error {
 		return err
 	}
 	fanartAuthor := fanart.GetString("author")
+	language := fanart.GetString("language")
 	author := data.GetString("username")
 	n.Set("content", author+" commented on your fanart")
 	n.Set("for_user", fanartAuthor)
 	n.Set("is_read", false)
-	n.Set("url", "/"+comment.Language+"/fanart/"+comment.TargetId)
+	n.Set("url", "/"+language+"/"+"fanart/"+comment.TargetId)
 	n.Set("source_user", comment.Author)
 	err = app.Save(n)
 	if err != nil {
@@ -81,11 +82,12 @@ func notifyOnFanfiction(app core.App, comment Comment) error {
 		return err
 	}
 	fanfictionAuthor := fanfiction.GetString("author")
+	language := fanfiction.GetString("language")
 	author := data.GetString("username")
 	n.Set("content", author+" commented on your fanfiction")
 	n.Set("for_user", fanfictionAuthor)
 	n.Set("is_read", false)
-	n.Set("url", "/"+comment.Language+"/fanfiction/"+comment.TargetId)
+	n.Set("url", "/"+language+"/"+"fanfiction/"+comment.TargetId)
 	n.Set("source_user", comment.Author)
 	err = app.Save(n)
 	if err != nil {
@@ -108,7 +110,7 @@ func notifyOnProfile(app core.App, comment Comment) error {
 	n.Set("content", author+" commented on your profile")
 	n.Set("for_user", comment.TargetId)
 	n.Set("is_read", false)
-	n.Set("url", "/"+comment.Language+"/profile?id="+comment.TargetId)
+	n.Set("url", "/"+"profile?id="+comment.TargetId)
 	n.Set("source_user", comment.Author)
 	err = app.Save(n)
 	if err != nil {
@@ -141,9 +143,9 @@ func notifyCommentParent(app core.App, comment Comment, commentType string) erro
 	var url string
 	switch commentType {
 	case "news":
-		url = "/" + comment.Language + "/news/" + comment.TargetId
+		url = "/" + "news/" + comment.TargetId
 	case "profile":
-		url = "/" + comment.Language + "/profile?id=" + comment.TargetId
+		url = "/" + "profile?id=" + comment.TargetId
 	}
 	n.Set("url", url)
 	n.Set("source_user", comment.Author)
