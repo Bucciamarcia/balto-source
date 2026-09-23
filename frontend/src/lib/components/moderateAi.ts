81faf6504a18e577bc116ee170c26858cafee764
Stefano Mini <info@stefanominiconsulting.com>
2026-09-23 16:15:27 +0300

    Jev broken

--- frontend/src/lib/components/moderateAi.ts
+++ frontend/src/lib/components/moderateAi.ts
@@ -1,26 +1,34 @@
 import { dev } from "$app/env";
 import { PUBLIC_POCKETBASE_URL } from "$lib/pocketbase/url";
+import { TypeSafeClient } from "@typesafe-ai/sdk";
+import { env } from "$env/dynamic/private";
 
 export async function moderateText(t: string, category?: string): Promise<ModerateResult> {
-	if (dev) {
-		console.log("Skipping in dev mode")
-		return "allow";
-	}
-	const response = await fetch(`${PUBLIC_POCKETBASE_URL}/moderate_text`, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({
-			"text": t, "category": category ?? ""
-		})
+	const k = env.TYPESAFE_API_KEY
+	console.log(k)
+	const client = new TypeSafeClient({
+		apiKey: k,
+
 	})
-	if (!response.ok) {
-		throw new Error(await response.text())
-	}
-	const r = await response.text()
-	const jsonResponse = JSON.parse(r)
-	const action = jsonResponse.action
-	const toReturn: ModerateResult = action
-	return toReturn
+	const resp = await client.systemOne({
+		model: "jev-latest",
+		state: t,
+		questions: {
+			safety: {
+				type: "choice",
+				instructions: "Is this content to be allowed under a PG rating?",
+				criteria: {
+					allow: "This content respects a PG rating",
+					moderate: "This content is at or slightly above PG rating",
+					remove: "This content is far beyond PG rating or is blatant spam",
+				},
+			},
+		},
+	})
+	const r = resp.answers.safety.choice
+	const conf = resp.answers.safety.confidence
+	console.log(r + "|" + conf)
+	return resp.answers.safety.choice
 }
 export async function moderateImageUrl(url: string): Promise<ModerateResult> {
 	if (dev) {
