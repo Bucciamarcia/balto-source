6b0e9705436203360e77abf18f44fa759f83f88f
Stefano Mini <info@stefanominiconsulting.com>
2026-09-25 18:09:37 +0300

    Tiptap for bio

--- frontend/src/routes/upload/CharacterUploader.svelte
+++ frontend/src/routes/upload/CharacterUploader.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import { enhance } from '$app/forms';
 	import FormError from '$lib/components/formError.svelte';
+	import TipTapEditor from '$lib/components/layout/comments/TipTapEditor.svelte';
 	import { m } from '$lib/paraglide/messages';
 	import type { CharacterSex } from './+page.server';
 	let characterSex: CharacterSex = $state('male');
 	let error: string = $state('');
 	let isLoading: boolean = $state(false);
 	let showSuccess: boolean = $state(false);
-	let files: FileList | undefined = $state();
+	let bio: string = $state('');
 </script>
 
 <h1>{m.upload_character_h()}</h1>
@@ -37,7 +38,7 @@
 	</fieldset>
 	<fieldset class="fieldset">
 		<legend class="fieldset-legend text-white">{m.upload_ch_pp_n()}</legend>
-		<input type="file" name="avatar" class="file-input text-black" bind:files />
+		<input type="file" name="avatar" class="file-input text-black" />
 		<label class="label" for="avatar">{m.upload_ch_max()}</label>
 	</fieldset>
 	<fieldset class="fieldset">
@@ -79,8 +80,8 @@
 		/>
 		<p class="text-white">{m.upload_ch_sex_o()}</p>
 	</div>
-	<textarea class="textarea mt-5 text-black" placeholder={m.upload_character_bio()} name="bio"
-	></textarea>
+	<TipTapEditor content="" header={m.upload_character_tt_h()} bind:value={bio} />
+	<input type="hidden" name="bio" bind:value={bio} />
 	<div>
 		{#if isLoading}
 			<span class="loading loading-md loading-spinner"></span>
