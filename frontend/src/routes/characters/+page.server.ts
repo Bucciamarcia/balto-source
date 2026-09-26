d91ca175fccf9eb9ef210825d38b1c917509f579
Stefano Mini <info@stefanominiconsulting.com>
2026-09-26 10:10:40 +0300

    Prettify

--- frontend/src/routes/characters/+page.server.ts
+++ frontend/src/routes/characters/+page.server.ts
@@ -1,19 +1,28 @@
-import { getLocale } from "$lib/paraglide/runtime";
-import type { CharacterFavoritesResponse, CharactersResponse, UsersResponse } from "$lib/pocketbase-types";
-import type { PageServerLoad } from "./$types";
+import { getLocale } from '$lib/paraglide/runtime';
+import type {
+	CharacterFavoritesResponse,
+	CharactersResponse,
+	UsersResponse
+} from '$lib/pocketbase-types';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	const characters = await locals.pb.collection("characters")
+	const characters = await locals.pb
+		.collection('characters')
 		.getFullList<CharactersResponse<{ owner: UsersResponse }>>({
-			expand: "owner",
-			filter: locals.pb.filter("language = {:language}", { language: getLocale() })
-		})
-	const allFavs = await locals.pb.collection("character_favorites")
+			expand: 'owner',
+			filter: locals.pb.filter('language = {:language}', { language: getLocale() })
+		});
+	const allFavs = await locals.pb
+		.collection('character_favorites')
 		.getFullList<CharacterFavoritesResponse>();
-	const favorites: Map<string, string[]> = new Map()
+	const favorites: Map<string, string[]> = new Map();
 	for (const c of characters) {
-		const cFavs = allFavs.filter((f) => f.target === c.id)
-		favorites.set(c.id, cFavs.map((cf) => cf.id))
+		const cFavs = allFavs.filter((f) => f.target === c.id);
+		favorites.set(
+			c.id,
+			cFavs.map((cf) => cf.id)
+		);
 	}
-	return { characters, favorites }
-}
+	return { characters, favorites };
+};
