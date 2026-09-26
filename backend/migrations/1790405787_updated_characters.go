50d8cd1329677c57315cfaa426ceda149e496e87
Stefano Mini <info@stefanominiconsulting.com>
2026-09-26 09:56:43 +0300

    PP char allowed formats

---
+++ backend/migrations/1790405787_updated_characters.go
@@ -0,0 +1,65 @@
+package migrations
+
+import (
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3298390430")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"help": "",
+			"hidden": false,
+			"id": "file4109890592",
+			"maxSelect": 0,
+			"maxSize": 0,
+			"mimeTypes": [
+				"image/png",
+				"image/jpeg"
+			],
+			"name": "ref_sheet",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": null,
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	}, func(app core.App) error {
+		collection, err := app.FindCollectionByNameOrId("pbc_3298390430")
+		if err != nil {
+			return err
+		}
+
+		// update field
+		if err := collection.Fields.AddMarshaledJSONAt(3, []byte(`{
+			"help": "",
+			"hidden": false,
+			"id": "file4109890592",
+			"maxSelect": 0,
+			"maxSize": 0,
+			"mimeTypes": null,
+			"name": "ref_sheet",
+			"presentable": false,
+			"protected": false,
+			"required": false,
+			"system": false,
+			"thumbs": null,
+			"type": "file"
+		}`)); err != nil {
+			return err
+		}
+
+		return app.Save(collection)
+	})
+}
